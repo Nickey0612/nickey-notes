173f795ed47741ca59275d4ee66f3281a602816a
nickey0612 <42453078+Nickey0612@users.noreply.github.com>
2026-07-24 18:04:06 +0900

    Add files via upload

--- scripts/menu.js
+++ scripts/menu.js
@@ -34,3 +34,32 @@ for (const link of snsMenu.querySelectorAll("a[href]")) {
     snsMenu.hidePopover();
   });
 }
+
+
+
+
+
+/*=============== SHOW MODAL ===============*/
+const openBtn = document.querySelector(".share__modal_btn");
+const modal = document.querySelector(".share__modal");
+
+if (openBtn && modal) {
+  openBtn.addEventListener("click", () => {
+    modal.classList.add("show-modal");
+  });
+}
+
+/*=============== CLOSE MODAL ===============*/
+const closeBtn = document.querySelector(".close_modal_btn");
+
+function closeModal() {
+  modal.classList.remove("show-modal");
+}
+closeBtn.addEventListener("click", closeModal);
+
+/*====== ESC BUTTON TO CLOSE MODAL ======*/
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    closeModal();
+  }
+});
