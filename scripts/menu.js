0bbbfedaabeb0d53a94a5a007be0f1072e5d0a90
nickey0612 <42453078+Nickey0612@users.noreply.github.com>
2026-07-30 18:09:39 +0900

    Add files via upload

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,17 +1,5 @@
 // モバイル用メニュー開閉
-const menu = document.querySelector("#menu");
-const buttonOpen = document.querySelector("#button-open");
-const buttonClose = document.querySelector("#button-close");
-
-buttonOpen.addEventListener("click", () => {
-  // dialogをtop layerへ表示
-  menu.showModal();
-});
-
-buttonClose.addEventListener("click", () => {
-  menu.close();
-});
-
+const menu = document.querySelector("#mobile-menu");
 menu.addEventListener("click", (event) => {
   // dialog本体クリックだけbackdrop扱い
   if (event.target === menu) {
@@ -27,42 +15,20 @@ for (const link of menu.querySelectorAll("a[href]")) {
 }
 
 // モバイル用SNSメニュー内リンク選択時のpopoverクローズ
-const snsMenu = document.querySelector("#menu-share");
-
-for (const link of snsMenu.querySelectorAll("a[href]")) {
-  link.addEventListener("click", () => {
-    snsMenu.hidePopover();
-  });
-}
-
-
-
-
-
-/*=============== SHOW MODAL ===============*/
-const openBtn = document.querySelector(".share__modal_btn");
-const modal = document.querySelector(".share__modal");
-
-if (openBtn && modal) {
-  openBtn.addEventListener("click", () => {
-    modal.classList.add("show-modal");
-  });
-}
-
-/*=============== CLOSE MODAL ===============*/
-const closeBtn = document.querySelector(".close_modal_btn");
-
-function closeModal() {
-  modal.classList.remove("show-modal");
-}
-closeBtn.addEventListener("click", closeModal);
-
-/*====== ESC BUTTON TO CLOSE MODAL ======*/
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") {
-    closeModal();
+const snsMenu = document.querySelector("#mobile-share-menu");
+snsMenu.addEventListener("click", (event) => {
+  // dialog本体クリックだけbackdrop扱い
+  if (event.target === snsMenu) {
+    snsMenu.close();
   }
 });
+
+// モバイル用SNSメニュー内リンクを押したら閉じる
+// for (const link of snsMenu.querySelectorAll("a[href]")) {
+//   link.addEventListener("click", () => {
+//     snsMenu.hidePopover();
+//   });
+// }
 
 // URLコピー完了の通知
 const toast = document.getElementById("copy_toast");
@@ -70,9 +36,7 @@ let toastTimer;
 
 function showCopyToast() {
   clearTimeout(toastTimer);
-
   toast.classList.add("show");
-
   toastTimer = setTimeout(() => {
     toast.classList.remove("show");
   }, 2000);
